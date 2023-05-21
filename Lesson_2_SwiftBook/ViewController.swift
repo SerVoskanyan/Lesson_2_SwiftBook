3a41b69c8f69aae194c06fce344ad02068e39c58
Ser <Ser@voskanjan.ru>
2023-05-22 02:10:40 +0400

    Initial Commit

---
+++ Lesson_2_SwiftBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson_2_SwiftBook
+//
+//  Created by Серик Восканян on 21.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
